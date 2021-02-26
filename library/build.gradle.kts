3ae361da39f406dc7432d555fad76246e1d796a0
seregil13 <alecerietman@gmail.com>
2021-02-26 09:16:02 -0800

    add maven publish

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     `java-library`
     kotlin("jvm")
     id("org.jmailen.kotlinter")
+    `maven-publish`
 }
 
 group = "com.seregil13"
@@ -16,10 +17,25 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
     testCompile("junit", "junit", "4.12")
-//    implementation("com.ironsource.aura.dslint:dslint-annotations:1.0.3")
 }
 
-//tasks.withType<KotlinCompile>() {
-//    kotlinOptions.apiVersion = "1.5"
-//    kotlinOptions.languageVersion = "1.5"
-//}
+publishing {
+    publications {
+        create<MavenPublication>("default") {
+            groupId = "com.seregil13"
+            artifactId = "graphql-dsl"
+            version = "1.0.0-alpha01"
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            name = "GithubPackages"
+            url = uri("https://maven.pkg.github.com/seregil13/graphql-dsl")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
